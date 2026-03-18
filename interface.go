2fbea38f15ea8bd5406f46f580828d1c9ef5e6c8
vampire321 <mh1295551@gmail.com>
2026-03-18 12:04:59 +0530

    learnt more about flag package (got stucked in use cases)

---
+++ interface.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+type Speaker interface {
+	Speak() string 
+}
+
+type Dog struct{}
+
+func (d Dog) Speak() string {
+	return "woof!"
+}
+
+type Cat struct{}
+
+func (c *Cat) Speak() string{
+	return "Meow!"
+}
+
+func inter() {
+	var s Speaker
+
+	d := Dog{}
+	s=d
+	fmt.Println(s.Speak())
+
+	c := Cat{}
+	s=&c
+	fmt.Println(s.Speak())
+
+}
--- main.go
+++ main.go
@@ -1,18 +1,9 @@
 package main
 import (
 	"flag"
-	"fmt"
 )
 func main(){
-	verbose := flag.Bool("verbose",false,"display logs")
-	flag.Parse()
-	if *verbose{
-		fmt.Println("Verbose mode ON: showing logs...")
-	} else {
-		fmt.Println("Verbose mode OFF ")
-	}
-	fmt.Println("value of port:", *nFlag)
-	fmt.Println("user:", *user)
+	flag.PrintDefaults()
 	variable()
 	structs()
 }
