4c05116c9ba375c79b68bff60fea246c34891560
vampire321 <mh1295551@gmail.com>
2026-03-23 23:24:15 +0530

    cleared the data race using mutex

--- race/main.go
+++ race/main.go
@@ -5,16 +5,21 @@ import(
 	"sync"
 )
 //data race on counter 
-var counter int
+var ( 
+	counter int
+	mu sync.Mutex
+)
 
 func increment(wg *sync.WaitGroup){
 	defer wg.Done()
-	counter++
+	mu.Lock() // accquire the lock - other goroutines block here
+	counter++ //safe -only one goroutine runs this at a time
+	mu.Unlock() //release the lock
 }
 
 func main(){
 	var wg sync.WaitGroup
-	for i := 0;i< 1000 ; i++{
+	for i := 0 ; i< 1000 ; i++{
 		wg.Add(1)
 		go increment(&wg)
 	}
