a7f54f0fec4ffb1b4cb23aac15ded913ed05a6fd
vampire321 <mh1295551@gmail.com>
2026-03-23 23:50:08 +0530

    Added channels folder

---
+++ channels/main.go
@@ -0,0 +1,32 @@
+//Sender and reciver go routines
+package main
+
+import "fmt"
+
+func producer(ch chan<- string){
+	// chan<- means "send-only channel" — can only send, not receive
+	urls := []string{"google.com", "github.com", "stripe.com"}
+	for _, url := range urls {
+        fmt.Println("sending:", url)
+		ch <- url //sernding url into the channel
+	}
+	close(ch) //signals:=no mre values will be sent
+}
+
+func consumer(ch <-chan string) {
+	// <-chan means "receive-only channel" - only can recieve
+	for url := range ch {
+		//Recives untill the channel is closed 
+		fmt.Println("recieved:", url)
+	}
+
+	fmt.Println("channel closed,consumer done")
+}
+
+func main() {
+	//made an unbufferd channel of strings
+	ch := make(chan string)
+
+	go producer(ch)
+	consumer(ch)
+}
