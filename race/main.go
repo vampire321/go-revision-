4884688a34cdf5835948368b87168f023b0f43bf
vampire321 <mh1295551@gmail.com>
2026-03-23 23:31:42 +0530

    wrote the reason for data race

--- race/main.go
+++ race/main.go
@@ -1,3 +1,4 @@
+//
 package main
 
 import(
@@ -5,6 +6,7 @@ import(
 	"sync"
 )
 //data race on counter 
+//when you counter++ the cpu actually performs 3 steps Read(go and get the v)-Modify(update 50 to 51)-Write(copy new value back into memory) eventually what happens is since we are spawning 1,000 workers, two or more workers try to do work at the same time 
 var ( 
 	counter int
 	mu sync.Mutex
