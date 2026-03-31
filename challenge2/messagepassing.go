e4be859d0912ab9336d0292cff10d564345e9a69
vampire321 <mh1295551@gmail.com>
2026-03-31 15:46:10 +0530

    basic channel

---
+++ challenge2/messagepassing.go
@@ -0,0 +1,21 @@
+package main
+
+import(
+	"fmt"
+)
+func sending(ch1 chan<- int){
+		nums := []int{1,2,3,4,5,6}
+		for num := range nums{
+			fmt.Println("sending:", num)
+			ch1 <- num
+		}
+		close(ch1)
+	}
+	func main(){
+	ch1 := make(chan int)
+	go sending(ch1)
+	for number := range ch1{
+		fmt.Println("received:", number)
+	}
+	fmt.Println("All numbers processed!")
+}
